e3fa6a6c4d47f88230f2289aa75d74ed962f452c
Vikil Naik <vikilnaik37@gmail.com>
2019-10-15 17:02:14 -0400

    Created a schema for posts/getting users, now working on the prisma server

--- src/index.js
+++ src/index.js
@@ -1,21 +1,37 @@
-const { GraphQLServer } = requre('graphqul-yoga');
+const { GraphQLServer } = require('graphql-yoga');
 
-// 1
-const typeDefs = `
-type Query {
-    info: String!
-}`
+
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}];
+
+let idCount = links.length;
 
 // 2 
 const resolvers = {
     Query: {
-        info: () => `This is the API of a Hackernews Clone`
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: () => links,
+    },
+
+    Mutation: {
+        post: (parent, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url,
+            }
+            links.push(link);
+            return link;
+        }
     }
-}
+};
 
 // 3
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers,
-})
+});
 server.start(() => console.log(`Server is running on http://localhost:4000`));
